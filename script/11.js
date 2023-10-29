7821c7cdc25edada9c3f1a8ce1e00b9ca7197027
rian911 <riansofc@gmail.com>
2023-10-29 20:57:38 +0700

    array and loop

--- script/11.js
+++ script/11.js
@@ -4,4 +4,7 @@ function addList() {
   const name = inputElement.value;
   todoList.push(name);
   console.log(todoList);
+
+  //   make input text clear again after click add button
+  inputElement.value = '';
 }
