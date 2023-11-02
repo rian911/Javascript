39f22a0afedc81de17117346749bf5da70a58ccc
rian911 <riansofc@gmail.com>
2023-11-02 17:18:45 +0700

    added date

--- script/11.js
+++ script/11.js
@@ -1,5 +1,14 @@
 // array penampungan todolist
-const todoList = ['pinjem', 'l00'];
+const todoList = [
+  {
+    name: 'pinjem',
+    dueDate: '2022-02-02',
+  },
+  {
+    name: 'helo',
+    dueDate: '2022-02-03',
+  },
+];
 
 // fungsi untuk menampilkan todolist
 renderTodo();
@@ -12,8 +21,10 @@ function renderTodo() {
   for (i = 0; i < todoList.length; i++) {
     // ini untuk penampungan nilai index didalam array
     const todo = todoList[i];
+    const name = todo.name;
+    const dueDate = todo.dueDate;
     // html digunakan untuk menampung string p + isi array
-    const html = `<p>${todo}<button onclick="todoList.splice(${i}, 1); renderTodo();">Delete</button></p>`;
+    const html = `<p>${name} ${dueDate}<button onclick="todoList.splice(${i}, 1); renderTodo();">Delete</button></p>`;
     // nah variabel let diatas tadi ditambah value html
     todolistHTML += html;
   }
@@ -24,14 +35,21 @@ function renderTodo() {
 function addList() {
   // make a input to a variable
   const inputElement = document.querySelector('.js-input');
+  const dateInput = document.querySelector('.js-date');
   const name = inputElement.value;
+  const date = dateInput.value;
 
   //   add value to array from input with add button
-  todoList.push(name);
+  todoList.push({
+    name: name,
+    dueDate: date,
+  });
+
   console.log(todoList);
 
   //   make input text clear again after click add button
   inputElement.value = '';
+  dateInput.value = '';
 
   // menjalankan fungsi diatas setiap kali add list baru
   renderTodo();
